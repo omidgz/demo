855216ed61659d47f58cdb00c69f9bd0c1e8aac1
Omid <omid@gholizadeh.ca>
2023-02-23 16:03:41 -0500

    playing w/ eslint

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Nav, Container} from 'react-bootstrap';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Nav>Welcome to my awesome demo app</Nav>
+			<Container className='p-3'>
+				<BrowserRouter>
+					<Routes>
+						<Route path='/' element={<h2>Index</h2>} />
+						<Route path='/test' element={<h2>TEST</h2>} />
+					</Routes>
+				</BrowserRouter>
+			</Container>
+		</div>
+	);
 }
 
 export default App;
---
+++ my-app/src/ShowUsers.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ShowUsers() {
+	return (
+		<div></div>
+	);
+}
+
+export default ShowUsers;
